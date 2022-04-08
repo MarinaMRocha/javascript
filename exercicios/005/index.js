fad04dad9b3caa8ad413aa3140770924e9da8c4a
MarinaMRocha <marinamr2021@gmail.com>
2022-04-08 18:31:47 -0300

    aprendendo e brincando com javascript :smile:

---
+++ exercicios/005/index.js
@@ -0,0 +1,7 @@
+var vel= 60
+console.log(`A velocidade do seu carro eh ${vel}Km/h`)
+if(vel > 60){ //Condicao simples
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+    console.log(`Favor respeitar as leis de transito.`)
+}
+console.log(`Dirija sempre usando cinto de seguranca!`)
---
+++ exercicios/005/index2.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+console.log(`Nasceu no(a) ${pais}`)
+if (pais == 'Brasil'){
+    console.log(`Voce eh Brasileiro(a)!`)
+}
+else{
+    console.log(`Voce eh Estrangeiro(a)!`)
+}
